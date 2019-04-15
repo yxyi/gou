d144d171293c05476a28ac6e2e87dd7718e5d357
yxyi <43663703+yxyi@users.noreply.github.com>
2019-04-15 15:45:00 +0800

    dom

---
+++ dom1/a.js
@@ -0,0 +1,5 @@
+function init(){
+  var divTag = document.document.getElementByID('fangkuang')
+  divTag.setAttribute('style','width:50px;height:15px;background-color:red;margin-top:400px;margin-left:400px')
+}
+window.onload = init
