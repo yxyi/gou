21695fcd8bf91f5e3f46e723ae0c3cc9681bf241
yxyi <43663703+yxyi@users.noreply.github.com>
2019-04-28 10:23:28 +0800

    console0428

---
+++ leixingzhuanhuan/aa.js
@@ -0,0 +1,14 @@
+console.log(whatEver);
+var whatEver=document.getElementById('aaa')
+console.log(whatEver)
+console.log(1/0)
+console.log(0/100)
+console.log('aa'*1);
+
+
+if(undefined===null)
+if(100==='100')
+if('1'===true)
+
+typeof(100)
+typeof('100')
